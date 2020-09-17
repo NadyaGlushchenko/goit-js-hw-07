824713e5733b61f738a51499c3652eb18f803b24
NadyaGlushchenko <Nadyaglushchenko@ukr.net>
2020-09-17 14:48:46 +0300

    добавляет решение задач

---
+++ task-1.js
@@ -0,0 +1,7 @@
+const sumСategoriesEl = document.querySelectorAll('.item');
+console.log(sumСategoriesEl);
+const titleEl = document.querySelectorAll('.title_js');
+
+console.log(titleEl[0].textContent);
+console.log(titleEl[1].textContent);
+console.log(titleEl[2].textContent);
